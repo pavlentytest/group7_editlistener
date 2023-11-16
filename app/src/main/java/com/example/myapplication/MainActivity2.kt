bbe48db409cd64a40f735e1fed1c52bea7965ce8
Paul <impetus@yandex.ru>
2023-11-16 17:04:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.editText.addTextChangedListener(
+            object: TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    if(p0.toString().isNotEmpty()) {
+                        val res = p0.toString().toDouble() * 100 + 678
+                        binding.textView.text = res.toString()
+                    } else {
+                        binding.textView.text = ""
+                    }
+                }
+
+            }
+        )
+
+    }
+}
